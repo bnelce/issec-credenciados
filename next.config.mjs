372ff7530778eca4f9ab38e5740a7632f591cd2b
Abner Oliveira <abneroliveiradev>
2024-02-03 10:09:24 -0300

    up

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,4 @@
-import { withContentlayer } from "next-contentlayer"
-
-import "./env.mjs"
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ["avatars.githubusercontent.com"],
-  },
-  experimental: {
-    appDir: true,
-    serverComponentsExternalPackages: ["@prisma/client"],
-  },
-}
+const nextConfig = {};
 
-export default withContentlayer(nextConfig)
+export default nextConfig;
