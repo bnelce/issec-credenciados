223c9cd38f5d2050ff1d0373480f4ef9f274adcd
Abner Oliveira <abneroliveiradev>
2024-03-06 19:18:36 -0300

    up

--- app/(marketing)/details/_components/DoctorSuggestionList.tsx
+++ app/(marketing)/details/_components/DoctorSuggestionList.tsx
@@ -1,98 +1,43 @@
 "use client";
 import { doctors } from "@/data/doctor-list";
-import { StaticImport } from "next/dist/shared/lib/get-img-props";
 import Image from "next/image";
 import Link from "next/link";
-import {
-  JSXElementConstructor,
-  Key,
-  PromiseLikeOfReactNode,
-  ReactElement,
-  ReactNode,
-  ReactPortal,
-} from "react";
 
 function DoctorSuggestionList() {
   return (
     <div className=" p-4 border-[1px] mt-5 md:ml-5 rounded-lg ">
       <h2 className="mb-3 font-bold">Suggestions</h2>
 
-      {doctors.map(
-        (
-          doctor: {
-            id: string;
-            attributes: {
-              image: { data: { attributes: { url: string | StaticImport } } };
-              categories: {
-                data: {
-                  attributes: {
-                    Name:
-                      | string
-                      | number
-                      | boolean
-                      | ReactElement<any, string | JSXElementConstructor<any>>
-                      | Iterable<ReactNode>
-                      | ReactPortal
-                      | PromiseLikeOfReactNode
-                      | null
-                      | undefined;
-                  };
-                }[];
-              };
-              Name:
-                | string
-                | number
-                | boolean
-                | ReactElement<any, string | JSXElementConstructor<any>>
-                | Iterable<ReactNode>
-                | ReactPortal
-                | PromiseLikeOfReactNode
-                | null
-                | undefined;
-              Year_of_Experience:
-                | string
-                | number
-                | boolean
-                | ReactElement<any, string | JSXElementConstructor<any>>
-                | Iterable<ReactNode>
-                | ReactPortal
-                | PromiseLikeOfReactNode
-                | null
-                | undefined;
-            };
-          },
-          index: Key | null | undefined
-        ) => (
-          <Link
-            key={index}
-            href={"/details/" + doctor.id}
-            className=" mb-4 p-3 shadow-sm w-full 
+      {doctors.map((doctor: any, index: number) => (
+        <Link
+          key={index}
+          href={"/details/" + doctor.id}
+          className=" mb-4 p-3 shadow-sm w-full 
             cursor-pointer hover:bg-slate-100
             rounded-lg flex items-center gap-3"
-          >
-            <Image
-              src={doctor.attributes?.image?.data?.attributes?.url}
-              width={70}
-              height={70}
-              className="w-[70px] h-[70px] rounded-full object-cover"
-              alt={""}
-            />
-            <div className="mt-3 flex-col flex gap-1 items-baseline">
-              <h2
-                className="text-[10px] bg-blue-100 p-1 rounded-full px-2
+        >
+          <Image
+            src={doctor.attributes?.image?.data?.attributes?.url}
+            width={70}
+            height={70}
+            className="w-[70px] h-[70px] rounded-full object-cover"
+            alt={""}
+          />
+          <div className="mt-3 flex-col flex gap-1 items-baseline">
+            <h2
+              className="text-[10px] bg-blue-100 p-1 rounded-full px-2
                      text-primary"
-              >
-                {doctor.attributes.categories?.data[0]?.attributes?.Name}
-              </h2>
-              <h2 className="font-medium text-sm">{doctor.attributes.Name}</h2>
-              <h2 className="text-primary text-xs flex gap-2">
-                {/* <GraduationCap/> */}
-                {doctor.attributes.Year_of_Experience}
-              </h2>
-            </div>
-          </Link>
-        )
-      )}
+            >
+              {doctor.attributes.categories?.data[0]?.attributes?.Name}
+            </h2>
+            <h2 className="font-medium text-sm">{doctor.attributes.Name}</h2>
+            <h2 className="text-primary text-xs flex gap-2">
+              {/* <GraduationCap/> */}
+              {doctor.attributes.Year_of_Experience}
+            </h2>
+          </div>
+        </Link>
+      ))}
     </div>
   );
 }
--- components/header.tsx
+++ components/header.tsx
@@ -2,34 +2,34 @@ import Image from "next/image";
 import Link from "next/link";
 import { Button } from "./ui/button";
 
-
 export function DashboardHeader() {
-
-  const Menu=[
+  const Menu = [
     {
-        id:1,
-        name:'Home',
-        path:'/'
+      id: 1,
+      name: "Home",
+      path: "/",
     },
     {
-        id:2,
-        name:'Explore',
-        path:'/explore'
+      id: 2,
+      name: "Explore",
+      path: "/explore",
     },
     {
-        id:3,
-        name:'Contact Us',
-        path:'/'
+      id: 3,
+      name: "Contact Us",
+      path: "/",
     },
-]
+  ];
   return (
-    <div className='flex items-center 
-    justify-between p-4 shadow-sm'>
-        <div className='flex items-center gap-10'>
-            <Image src='/logo.svg' alt='logo'
-            width={180} height={80}
-            />
-            {/* <ul className='md:flex gap-8 hidden'>
+    <div
+      className="flex items-center 
+    justify-between p-4 shadow-sm"
+    >
+      <div className="flex items-center gap-10">
+        <Link href="/">
+          <Image src="/logo.svg" alt="logo" width={180} height={80} />
+        </Link>
+        {/* <ul className='md:flex gap-8 hidden'>
                 {Menu.map((item,index)=>(
                     <Link href={item.path} key={index}>
                     <li className='hover:text-primary
@@ -38,11 +38,9 @@ export function DashboardHeader() {
                     </Link>
                 ))}
             </ul> */}
-        </div>
-      
-        <Button>Pague-me um café</Button>
-      
-        
+      </div>
+
+      <Button>Pague-me um café</Button>
     </div>
-  )
+  );
 }
